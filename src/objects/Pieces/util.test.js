90fc4213ac372f7d755835756cfabe1f75e21ff2
Mohmn <naqashmohmin1@gmail.com>
2022-12-19 02:38:02 +0530

    fix: util test

--- src/objects/Pieces/util.test.js
+++ src/objects/Pieces/util.test.js
@@ -26,37 +26,37 @@ describe('test util traversing funcs', () => {
 
     it('vertical traverse up test', () => {
         const moves = traverseUpVerticaly(2, 5);
-        const expectedMoves = [[3, 5], [4, 5], [5, 5], [6, 5], [7, 5]];
+        const expectedMoves = [[1, 5], [0, 5]];
         expect(moves).toEqual(expectedMoves);
     })
 
     it('vertical traverse down test', () => {
         const moves = traverseDownVerticaly(2, 5);
-        const expectedMoves = [[1, 5], [0, 5]];
+        const expectedMoves = [[3, 5], [4, 5], [5, 5], [6, 5], [7, 5]];
         expect(moves).toEqual(expectedMoves);
     })
 
     it('diagonal traverse right down test', () => {
         const moves = traversRightDownDiagonaly(3, 3);
-        const expectedMoves = [[4, 4], [6, 6], [7, 7]];
+        const expectedMoves = [[4, 4], [5, 5], [6, 6], [7, 7]];
         expect(moves).toEqual(expectedMoves);
     })
 
     it('diagonal traverse right up test', () => {
         const moves = traversRightUpDiagonaly(3, 5);
-        const expectedMoves = [[2, 4], [1, 3]];
+        const expectedMoves = [[2, 4], [1, 3],[0,2]];
         expect(moves).toEqual(expectedMoves);
     })
 
     it('diagonal traverse left down test', () => {
         const moves = traversLeftDownDiagonaly(3, 5);
-        const expectedMoves = [[5, 4], [6, 4], [7, 3]];
+        const expectedMoves = [[4, 4], [5, 3], [6, 2],[7,1]];
         expect(moves).toEqual(expectedMoves);
     })
 
     it('diagonal traverse left up test', () => {
         const moves = traversLeftUpDiagonaly(3, 5);
-        const expectedMoves = [[2, 5], [1, 6], [0, 7]];
+        const expectedMoves = [[2, 6], [1, 7]];
         expect(moves).toEqual(expectedMoves);
     })
 
